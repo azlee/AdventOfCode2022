de2da7e421ec636e2bd4c05ee1901b7febf8dceb
Amber Lee <amber_Lee1993@hotmail.com>
2022-12-11 15:28:06 -0800

    day 8 part 2

--- day8.ts
+++ day8.ts
@@ -1,6 +1,7 @@
 // https://adventofcode.com/2022/day/8
 
 import * as fs from "fs";
+require("util");
 
 function parseGrid(input: string): number[][] {
   const lines = input.split("\n");
@@ -55,6 +56,55 @@ function isVisible(grid: number[][], row: number, col: number): boolean {
   return isVisible;
 }
 
+function getViewingDistance(
+  grid: number[][],
+  row: number,
+  col: number,
+  dir: number[]
+): number {
+  const [x, y] = dir;
+  let i = row + x;
+  let j = col + y;
+  let prevTreeHeight = 0;
+  let viewingDistance = 0;
+  while (i >= 0 && j >= 0 && i < grid.length && j < grid[0].length) {
+    viewingDistance++;
+    prevTreeHeight = grid[i][j];
+    if (prevTreeHeight >= grid[row][col]) {
+      break;
+    }
+    i += x;
+    j += y;
+  }
+  return viewingDistance;
+}
+
+function getScenicScore(grid: number[][], row: number, col: number): number {
+  let score = 1;
+  for (const dir of [
+    [-1, 0],
+    [0, -1],
+    [1, 0],
+    [0, 1],
+  ]) {
+    score *= getViewingDistance(grid, row, col, dir);
+  }
+  return score;
+}
+
+function getHighestScenicScore(): number {
+  const input = fs.readFileSync("day8input.txt", "utf8");
+  const grid: number[][] = parseGrid(input);
+  let highestScenicScore = 0;
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
+      const scenicScore = getScenicScore(grid, i, j);
+      highestScenicScore = Math.max(scenicScore, highestScenicScore);
+    }
+  }
+  return highestScenicScore;
+}
+
 function getNumVisibleTrees(): number {
   const input = fs.readFileSync("day8input.txt", "utf8");
   const grid: number[][] = parseGrid(input);
@@ -70,3 +120,4 @@ function getNumVisibleTrees(): number {
 }
 
 console.log("Part 1: ", getNumVisibleTrees());
+console.log("Part 2: ", getHighestScenicScore());
