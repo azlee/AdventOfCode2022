54b163cf6aaf5f3d63f8b58428e066721f87c7f0
Amber Lee <amber_Lee1993@hotmail.com>
2023-11-24 14:46:29 -0800

    day 15 part 2

--- day15.ts
+++ day15.ts
@@ -12,12 +12,12 @@ function parseInput(): number[][][] {
     let [_, _a, x, y] = sensorLine.split(" ");
     let [_2, _a2, _b, _c, x2, y2] = beaconLine.split(" ");
     const sensorPos = [
-      parseInt(y.split("=")[1]),
       parseInt(x.split("=")[1].slice(0, -1)),
+      parseInt(y.split("=")[1]),
     ];
     const beaconPos = [
-      parseInt(y2.split("=")[1]),
       parseInt(x2.split("=")[1].slice(0, -1)),
+      parseInt(y2.split("=")[1]),
     ];
     beacons.push(beaconPos);
     sensors.push(sensorPos);
@@ -26,15 +26,15 @@ function parseInput(): number[][][] {
   return [beacons, sensors];
 }
 
-function calculatePositionsWithNoBeacon(beacons, sensors, row): number {
+function calculatePositionsWithNoBeaconInRow(beacons, sensors, row): number {
   const noBeaconInRow: Set<number> = new Set(); // all the x positions in row where they are no beacons
   for (let i = 0; i < beacons.length; i++) {
     const beacon = beacons[i];
     const sensor = sensors[i];
     const distance =
       Math.abs(beacon[0] - sensor[0]) + Math.abs(sensor[1] - beacon[1]);
-    for (let x = sensor[1] - distance; x <= sensor[1] + distance; x++) {
-      if (Math.abs(x - sensor[1]) + Math.abs(sensor[0] - row) <= distance) {
+    for (let x = sensor[0] - distance; x <= sensor[0] + distance; x++) {
+      if (Math.abs(x - sensor[0]) + Math.abs(sensor[1] - row) <= distance) {
         noBeaconInRow.add(x);
       }
     }
@@ -42,16 +42,87 @@ function calculatePositionsWithNoBeacon(beacons, sensors, row): number {
 
   for (let i = 0; i < beacons.length; i++) {
     const beacon = beacons[i];
-    if (beacon[0] === row) {
+    if (beacon[1] === row) {
       noBeaconInRow.delete(beacon[1]);
     }
   }
   return noBeaconInRow.size;
 }
 
-function foo(): number {
+function getPointsToCheck(beacons, sensors, low, high): number[][] {
+  // check the perimeter of each diamond
+  const deltas = [
+    [-1, -1],
+    [1, 1],
+    [-1, 1],
+    [1, -1],
+  ];
+  // top, bottom, right, left
+  const edges = [
+    [0, 1],
+    [0, -1],
+    [1, 0],
+    [-1, 0],
+  ];
+  const pointsToCheck: number[][] = [];
+  for (let b = 0; b < beacons.length; b++) {
+    const beacon = beacons[b];
+    const sensor = sensors[b];
+    const distance =
+      Math.abs(beacon[0] - sensor[0]) + Math.abs(sensor[1] - beacon[1]);
+    for (let i = 0; i < 4; i++) {
+      let edge = [
+        sensor[0] + edges[i][0] * (distance + 1),
+        sensor[1] + edges[i][1] * (distance + 1),
+      ];
+      const [dx, dy] = deltas[i];
+      let pointNum = 0;
+      while (pointNum <= distance) {
+        const newPoint = [dx * pointNum + edge[0], dy * pointNum + edge[1]];
+        if (
+          newPoint[0] >= low &&
+          newPoint[0] <= high &&
+          newPoint[1] >= low &&
+          newPoint[1] <= high
+        ) {
+          pointsToCheck.push(newPoint);
+        } else {
+          break;
+        }
+        pointNum++;
+      }
+    }
+  }
+  return pointsToCheck;
+}
+
+function findTuningFrequencyInRange(beacons, sensors, low, high): number {
+  const pointsToCheck = getPointsToCheck(beacons, sensors, low, high);
+  console.log("pointsToCheck len", pointsToCheck.length);
+  for (const point of pointsToCheck) {
+    let i = 0;
+    for (; i < beacons.length; i++) {
+      const beacon = beacons[i];
+      const sensor = sensors[i];
+      const distance =
+        Math.abs(beacon[0] - sensor[0]) + Math.abs(beacon[1] - sensor[1]);
+      const pointDistance =
+        Math.abs(point[0] - sensor[0]) + Math.abs(point[1] - sensor[1]);
+      if (pointDistance <= distance) {
+        break;
+      }
+    }
+    if (i === beacons.length) {
+      console.log("point", point);
+      return point[0] * 4000000 + point[1];
+    }
+  }
+  return 0;
+}
+
+function part1(): number {
   const [beacons, sensors] = parseInput();
-  const numPositionsNoBeacon = calculatePositionsWithNoBeacon(
+  const numPositionsNoBeacon = calculatePositionsWithNoBeaconInRow(
     beacons,
     sensors,
     2000000
@@ -59,4 +130,16 @@ function foo(): number {
   return numPositionsNoBeacon;
 }
 
-console.log("Part 1: ", foo());
+function part2(): number {
+  const [beacons, sensors] = parseInput();
+  const positionWithBeacon = findTuningFrequencyInRange(
+    beacons,
+    sensors,
+    0,
+    4000000
+  );
+  return positionWithBeacon;
+}
+
+console.log("Part 1: ", part1());
+console.log("Part 2: ", part2());
